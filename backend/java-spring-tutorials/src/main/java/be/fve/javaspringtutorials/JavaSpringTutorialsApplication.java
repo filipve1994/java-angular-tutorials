7cdc560defad2cd61fe307a11850ff4eacb9af9a
Filip Vanden Eynde <filipve1994@gmail.com>
2023-01-10 22:35:36 +0100

    fve: add base backend frontend structure

---
+++ backend/java-spring-tutorials/src/main/java/be/fve/javaspringtutorials/JavaSpringTutorialsApplication.java
@@ -0,0 +1,13 @@
+package be.fve.javaspringtutorials;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringTutorialsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringTutorialsApplication.class, args);
+	}
+
+}
